31f35bb851f09601f0c50e822a50df19ac1f0456
鲍锐 <baorui@s-Macbook-Air.local>
2024-10-13 15:33:36 -0400

    change app.py

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ def search():
     return jsonify({'documents': documents, 'similarities': similarities, 'indices': indices}) 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=3000)
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=3000)
